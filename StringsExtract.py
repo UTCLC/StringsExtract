91f4f150f8bd72e962dc67d495896da07d7fd1aa
SheepYhangCN <sheepyuhang@163.com>
2025-04-04 18:38:35 +0800

    添加了 try 来防止二进制文件中断 & 删除了 sort_keys 来保留原有顺序

--- StringsExtract.py
+++ StringsExtract.py
@@ -8,28 +8,31 @@ def find(dir):
 		path = os.path.join(dir, dirfile)
 		if (os.path.isfile(path) and not (path.endswith("strings.json"))):
 			print("Searching "+path)
-			with (open(path, mode="r", encoding="utf-8") as f):
-				lines = f.readlines()
-				linen = 0
-				for line in lines:
-					searchs = re.finditer(pattern,line)
-					found = False
-					num = 0
-					for search in searchs:
-						found = True
-						string = search.group().replace("\"","")
-						print(f"Found {string} in {path} at line {linen}, num {num}")
-						strings[path.replace(directory,"").lstrip("\\")+":"+str(linen)+":"+str(num)] = string
-						num += 1
-					linen += 1
-				if (found):
-					print(f"No string was found in {path}")
+			try: 
+				with (open(path, mode="r", encoding="utf-8") as f):
+					lines = f.readlines()
+					linen = 0
+					for line in lines:
+						searchs = re.finditer(pattern,line)
+						found = False
+						num = 0
+						for search in searchs:
+							found = True
+							string = search.group().replace("\"","")
+							print(f"Found {string} in {path} at line {linen}, num {num}")
+							strings[path.replace(directory,"").lstrip("\\")+":"+str(linen)+":"+str(num)] = string
+							num += 1
+						linen += 1
+					if (found):
+						print(f"No string was found in {path}")
+			except:
+				print("Error encountered when loading "+path)
 		elif (os.path.isdir(path)):
 			find(path)
 
 def output(dir):
 	with open(dir+"/strings.json", mode="w", encoding="utf-8") as f:
-		f.write(json.dumps(strings, sort_keys=True, indent=4, separators=(',', ': ')))
+		f.write(json.dumps(strings, indent=4, separators=(',', ': ')))
 
 def inputt(jsonf):
 	global strings
@@ -51,21 +54,27 @@ def write(dir):
 		m = "r+"
 		if (not os.path.exists(dir+"Repacked/"+path)):
 			m = "w+"
-			with (open(dir+path, mode="r", encoding="utf-8") as f):
-				cont = f.readlines()
-		with (open(dir+"Repacked/"+path, mode=m, encoding="utf-8") as ff):
-			if (cont == None):
-				lines = ff.readlines()
-			else:
-				ff.writelines(cont)
-				lines = cont
-			ff.seek(0)
-			line = lines[linen]
-			searchs = re.findall(pattern,line)
-			if (len(searchs) != 0):
-				line.replace(searchs[num],strings[file],1)
-			else:
-				print(f"No string was found in {path}")
+			try:
+				with (open(dir+path, mode="r", encoding="utf-8") as f):
+					cont = f.readlines()
+			except:
+				print("Error encountered when loading "+dir+path)
+		try:
+			with (open(dir+"Repacked/"+path, mode=m, encoding="utf-8") as ff):
+				if (cont == None):
+					lines = ff.readlines()
+				else:
+					ff.writelines(cont)
+					lines = cont
+				ff.seek(0)
+				line = lines[linen]
+				searchs = re.findall(pattern,line)
+				if (len(searchs) != 0):
+					line.replace(searchs[num],strings[file],1)
+				else:
+					print(f"No string was found in {path}")
+		except:
+			print("Error encountered when loading "+dir+"Repacked/"+path)
 
 strings = {}
 directory = input("Directory: ").replace("\\","/")
